dbc0e4ffb2bfcc99540d75d171bff5a484bd8cde
Totta Wang <totta.wang@autodesk.com>
2017-07-29 10:43:31 +0800

    remove unnecessary customizer list bean

--- src/main/java/com/sample/conf/CacheCustomizerConfig.java
+++ src/main/java/com/sample/conf/CacheCustomizerConfig.java
@@ -1,8 +1,6 @@
 package com.sample.conf;
 
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -20,17 +18,7 @@ public class CacheCustomizerConfig {
 
   public static final String CACHE_NAME_USERS = "users";
 
-  /**
-   * <code>org.springframework.boot.autoconfigure.cache.CacheManagerCustomizers<code> can only
-   * inject list rather than single cache manager customizer.
-   * 
-   * @return
-   */
   @Bean
-  public List<CacheManagerCustomizer<RedisCacheManager>> cacheManagerCustomizers() {
-    return Collections.singletonList(cacheManagerCustomizer());
-  }
-
   public CacheManagerCustomizer<RedisCacheManager> cacheManagerCustomizer() {
     return cacheManager -> {
       final Map<String, Long> expires = new HashMap<>();
